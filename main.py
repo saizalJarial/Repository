a0b5275eecefb5cb2045737909ee3c5497480c49
saizalJarial <saizal.jarial@g-ushin.com>
2025-05-29 11:53:23 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+"""This module contains greeting functions."""
+
+
+def hello():
+    """Print a greeting."""
+    print("hi")
+
+
+def bye():
+    """Print a farewell."""
+    print("bye")
+
+
+print(hello())
